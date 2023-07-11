79ef37407a68ae27e753ad36355217a5aec31f56
Sumant Bag <86064843+Sumantbag1992@users.noreply.github.com>
2023-07-11 11:33:19 +0000

    Update base-urls.ts

--- src/app/base-urls.ts
+++ src/app/base-urls.ts
@@ -1,6 +1,6 @@
 export class BaseUrls {
 
-  public static readonly BASE_HREF: string = "http://localhost:8080";
+  public static readonly BASE_HREF: string = "http://ec2-54-173-193-142.compute-1.amazonaws.com:8080";
 
   public static readonly ADMIN_GROUPURL: string = "adminauth";
   public static readonly USER_GROUPURL: string = "users";
